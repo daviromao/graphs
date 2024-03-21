29fe92e996cae4f1ace15f17d8eb01684569d042
Davi Romão <davi.romao0210@gmail.com>
2024-03-21 14:34:22 -0300

    kosaraju

--- kosaraju/kosaraju.cpp
+++ kosaraju/kosaraju.cpp
@@ -51,10 +51,52 @@ void Kosaraju(Graph G, Graph Gr, vector<vertex> &post) {
     }
 }
 
-int main() {
-    int n, m;
+int main(int argc, char const *argv[])
+{
+    int opt;
 
-    cin >> n >> m;
+    string in_name = "";
+    string out_name = "";
+
+    while ((opt = getopt(argc, (char *const *)argv, "ho:f:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'h':
+            cout << "Help:\n"
+                 << "-h           : mostra o help\n"
+                 << "-o <arquivo> : redireciona a saida para o \"arquivo\"\n"
+                 << "-f <arquivo> : indica o \"arquivo\" que contém o grafo de entrada\n";
+            return 0;
+        case 'f':
+            in_name = optarg;
+            break;
+        case 'o':
+            out_name = optarg;
+            break;
+            break;
+        default:
+            cerr << "Use: " << argv[0] << " [-h] [-o output_file] [-f input_file]\n";
+            return EXIT_FAILURE;
+        }
+    }
+
+    if (in_name == "")
+    {
+        cerr << "Especifique o arquivo de entrada! Utilize: -f" << endl;
+        return EXIT_FAILURE;
+    }
+
+    ifstream file_in(in_name);
+
+    if (not file_in)
+    {
+        cerr << "Não foi possível abrir o arquivo: " << in_name << endl;
+        return EXIT_FAILURE;
+    }
+
+    int n, m;
+    file_in >> n >> m;
 
     vector<vertex> post(n, -1);
     vector<vertex> sc(n, -1);
@@ -63,13 +105,27 @@ int main() {
     
     for(int i = 0; i < m; i++){
         int u, v;
-        cin >> u >> v;
+        file_in >> u >> v;
         u--, v--;
         addEdge(G, u, v, 0);
         addEdge(Gr, v, u, 0); // reverse graph
     }
     
+    file_in.close();
+
     Kosaraju(G, Gr, post);
+
+    ofstream file_out;
+    if (out_name != "")
+    {
+        file_out.open(out_name);
+        if (!file_out)
+        {
+            cerr << "Não foi possível gravar no arquivo: " << out_name << endl;
+            return EXIT_FAILURE;
+        }
+        cout.rdbuf(file_out.rdbuf());
+    }
 
     for(auto cc:components){
         for(auto v:cc){
@@ -77,5 +133,6 @@ int main() {
         }
         cout << endl;
     }
-    
+
+    file_out.close();
 }
